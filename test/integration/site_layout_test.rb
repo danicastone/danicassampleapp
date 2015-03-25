78051d4eb6393de2c804efdfaa6b6e8384276e85
Danica Nuccitelli <danicastone@gmail.com>
2015-03-25 23:22:01 +0000

    adding index to user's email

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,7 +1,6 @@
 require 'test_helper' 
 
 class SiteLayoutTest < ActionDispatch::IntegrationTest
-
   test "layout links" do
     get root_path
     assert_template 'static_pages/home'
@@ -10,6 +9,6 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
     get signup_path
-    assert_select "title", full_title "Sign Up"
+    assert_select "title", full_title("Sign up")
   end
 end
